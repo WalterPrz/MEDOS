c424279586266560715f27681a359e892f1257c1
JosueCuellar <cc19114@ues.edu.sv>
2022-09-17 17:12:56 -0600

    Agregando migraciones

---
+++ database/migrations/2022_09_17_014837_create_expedientes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expedientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombrePaciente',100);
+            $table->integer('edadPaciente');
+            $table->string('genero',10);
+            $table->string('telefonoPaciente',10);
+            $table->string('alergias',255)->nullable();
+            $table->date('fechaApertura');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expedientes');
+    }
+};
