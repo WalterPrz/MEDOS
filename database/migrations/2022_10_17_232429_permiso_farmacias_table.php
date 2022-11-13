f3451b1dc78ca224adc56ae23556b7cb77881d8a
PT18003 <pt18003@ues.edu.sv>
2022-11-13 10:47:39 -0600

    vista de examenes por paciente

--- database/migrations/2022_10_17_232429_permiso_farmacias_table.php
+++ database/migrations/2022_10_17_232429_permiso_farmacias_table.php
@@ -14,18 +14,10 @@ return new class extends Migration
      */
     public function up()
     {
-<<<<<<<< HEAD:database/migrations/2022_10_17_232429_permiso_farmacias_table.php
         Schema::create('permiso_farmacias', function (Blueprint $table) {
             $table->id();
             $table->string('nombrePermisoFarm',2048);
             $table->decimal('monto',5,2);
-========
-        Schema::create('abonos', function (Blueprint $table) {
-            $table->id();
-            $table->foreignIdFor(Credito::class)->references('id')->on('creditos');
-            $table->decimal('cantidadAbonada', $precision = 5, $scale = 2)->nullable();
-            $table->string('fechaAbono',10)->nullable();
->>>>>>>> 909b0c87f272d85cda0209e265318a4e8214ec13:database/migrations/2022_10_18_112320_create_abonos_table.php
             $table->timestamps();
             $table->softDeletes();
         });
@@ -38,10 +30,9 @@ return new class extends Migration
      */
     public function down()
     {
-<<<<<<<< HEAD:database/migrations/2022_10_17_232429_permiso_farmacias_table.php
+
         Schema::dropIfExists('permiso_farmacias');
-========
-        Schema::dropIfExists('abonos');
->>>>>>>> 909b0c87f272d85cda0209e265318a4e8214ec13:database/migrations/2022_10_18_112320_create_abonos_table.php
+
+
     }
 };
